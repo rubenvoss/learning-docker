dc2354fd37baa59db4db1b047b069fd4aa2c8038
Ruben Voss <bekertshop@gmail.com>
2022-09-15 15:24:52 +0200

    changes localhost into container name

--- multi-01-starting-setup/frontend/src/App.js
+++ multi-01-starting-setup/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://node_container/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://node_container/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch('http://node_container/goals/' + goalId, {
         method: 'DELETE',
       });
 
